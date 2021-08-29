10d527a90a2600b67ba30b1795eb0103cf71a5e6
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 19:33:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionDemo
+{
+    class ArrayListCollection
+    {
+        static void Main(string[] args)
+        {
+            ArrayList Al = new ArrayList();
+
+            Al.Add(2);
+            Al.Add("Jadhav");
+            Al.Add(true);
+            Al.Add('P');
+            ArrayListDemo DAL = new ArrayListDemo();
+            DAL.PrintArrList(Al);
+            IEnumerator Ien = Al.GetEnumerator();
+            while (Ien.MoveNext())
+            {
+                Console.Write("\t{0}", Ien.Current);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public class ArrayListDemo
+    {
+        //public void ArrListMethod()
+        //{
+            
+        //    //PrintArrList(Al);
+        //}
+
+    
+    
+        public void PrintArrList(IEnumerable AL)
+        {
+            System.Collections.IEnumerator Ien = AL.GetEnumerator();
+            while (Ien.MoveNext())
+            {
+                Console.Write("\t{0}", Ien.Current);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    
+
+}
